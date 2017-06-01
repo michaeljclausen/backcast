6074cd1f51f0e2761ff251a4011f0de2bd7b4730
Hack Reactor Student <student@hackreactor.com>
2017-06-01 12:14:49 -0700

    click handling for title and thumbnail complete

--- src/views/videoListEntry.js
+++ src/views/videoListEntry.js
@@ -6,8 +6,17 @@ var VideoListEntryView = Backbone.View.extend({
 
   render: function() {
     this.$el.html(this.template());
+
     this.$el.find('.media-object').attr('src', this.model.attributes.thumbnail);
+    this.$el.find('.media-object').on('click', el => {
+      //console.log(this);
+      var newVideoToBePlayed = new VideoPlayerView({model: this.model});
+    });
+
     this.$el.find('.video-list-entry-title').text(this.model.attributes.title);
+    this.$el.find('.video-list-entry-title').on('click', el => {
+      var newVideoToBePlayed = new VideoPlayerView({model: this.model});  
+    });
     this.$el.find('.video-list-entry-detail').text(this.model.attributes.description);
     return this.$el;
   },
